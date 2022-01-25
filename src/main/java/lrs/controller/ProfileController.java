468774e75a29c3f7c020d90ac543fdc79fa469ca
mumu <2445603864@qq.com>
2022-01-25 21:33:15 +0800

    1

---
+++ src/main/java/lrs/controller/ProfileController.java
@@ -0,0 +1,13 @@
+package lrs.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ProfileController {
+    @RequestMapping(value = "/profile")
+    public String toProfile(){
+        return "profile";
+    }
+}
